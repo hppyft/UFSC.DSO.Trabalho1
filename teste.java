926f78d07abd28963920073d7bce5654844553d3
hppyft <leo@wiitec.com.br>
2016-05-15 19:35:09 -0300

    Create teste.java

---
+++ teste.java
@@ -0,0 +1,33 @@
+
+package ufsc.dso.trabalho1;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.Calendar;
+
+
+public class teste {
+ 
+    public static void main (String[] Args) throws ParseException{
+        
+        /*
+        int a = 1995;
+        Calendar dataNascimento = Calendar.getInstance();
+        dataNascimento.set(Calendar.YEAR,a);
+        System.out.println(dataNascimento.get(Calendar.YEAR));
+        */
+        /*
+        SimpleDateFormat datos = new SimpleDateFormat("HH:mm");
+        String dataString = "20:30";
+        Date data = datos.parse(dataString);
+        System.out.println(data);
+        */
+        /*
+        Calendar data = Calendar.getInstance();
+        data.set(Calendar.HOUR_OF_DAY, 17);
+        data.set(Calendar.MINUTE, 28);
+        System.out.println(data.get(data.HOUR_OF_DAY)+":"+data.get(data.MINUTE));
+        */
+    }
+    
+}
