65f3c6b0572d057ae6d3c7e276bf82ea44073f6e
hppyft <leo@wiitec.com.br>
2016-05-23 02:23:45 -0300

    Update teste.java

--- teste.java
+++ teste.java
@@ -1,14 +1,27 @@
-
+/*
+PERGUNTAR:
+O QUE É MELHOR, FAZER UMA LISTA OU HASHMAP DE CARGOS?
+HASHMAP OU HASHSET?
+*/
 package ufsc.dso.trabalho1;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
 import java.util.Date;
 import java.util.Calendar;
+import java.util.Scanner;
+import ufsc.dso.trabalho1.Controladores.ControladorGeral;
 
 
 public class teste {
+    
+    private static boolean fecharSistema = false;
  
     public static void main (String[] Args) throws ParseException{
+        
+        while (!isFecharSistema()){
+            ControladorGeral.getInstance().inicializa();
+        }
+        
         
         /*
         int a = 1995;
@@ -28,6 +41,14 @@ public class teste {
         data.set(Calendar.MINUTE, 28);
         System.out.println(data.get(data.HOUR_OF_DAY)+":"+data.get(data.MINUTE));
         */
+    }
+
+    public static boolean isFecharSistema() {
+        return fecharSistema;
+    }
+
+    public static void setFecharSistema(boolean fecharSistema) {
+        teste.fecharSistema = fecharSistema;
     }
     
 }
