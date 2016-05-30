9ada060ac74f3650e62ea3af6793334f1fc97c18
hppyft <leo@wiitec.com.br>
2016-05-30 01:33:45 -0300

    Update TelaControladorFuncionario.java

--- TelaControladorFuncionario.java
+++ TelaControladorFuncionario.java
@@ -10,49 +10,41 @@ public class TelaControladorFuncionario {
     }
 
     public int telaCadastroFuncionarioMatricula() {
-        scanner.nextLine();
         System.out.println("Digite a matricula do funcionario");
         return scanner.nextInt();
     }
 
     public String telaCadastroFuncionarioNome() {
-        scanner.nextLine();
         System.out.println("Digite o nome do funcionario");
         return scanner.nextLine();
     }
 
     public int telaCadastroFuncionarioAnoNascimento() {
-        scanner.nextLine();
         System.out.println("Digite o ano de nascimento do funcionario");
         return scanner.nextInt();
     }
 
     public int telaCadastroFuncionarioMesNascimento() {
-        scanner.nextLine();
         System.out.println("Digite o mes de nascimento do funcionario");
         return scanner.nextInt();
     }
 
     public int telaCadastroFuncionarioDiaNascimento() {
-        scanner.nextLine();
         System.out.println("Digite o dia de nascimento do funcionario");
         return scanner.nextInt();
     }
 
     public int telaCadastroFuncionarioTelefone() {
-        scanner.nextLine();
         System.out.println("Digite o telefone do funcionario");
         return scanner.nextInt();
     }
 
     public int telaCadastroFuncionarioSalario() {
-        scanner.nextLine();
         System.out.println("Digite o salario do funcionario");
         return scanner.nextInt();
     }
 
     public Integer telaCadastroFuncionarioCodigoDoCargo() {
-        scanner.nextLine();
         System.out.println("Digite o nome do cargo do funcionario");
         return scanner.nextInt();
     }
