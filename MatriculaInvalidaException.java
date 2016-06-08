9402ed10d15f001203ddaec08f75f5dee257d0eb
hppyft <leo@wiitec.com.br>
2016-06-08 03:49:29 -0300

    Create MatriculaInvalidaException.java

---
+++ MatriculaInvalidaException.java
@@ -0,0 +1,10 @@
+
+package ufsc.dso.trabalho1.Entidades;
+
+public class MatriculaInvalidaException extends Exception{
+
+    public MatriculaInvalidaException(String message) {
+        super(message);
+    }
+    
+}
