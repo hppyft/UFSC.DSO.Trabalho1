2efc15aacb3dcbbbf5ea065f4bfed4faacb1a1d4
hppyft <leo@wiitec.com.br>
2016-06-08 03:49:46 -0300

    Create IntervaloInvalidoException.java

---
+++ IntervaloInvalidoException.java
@@ -0,0 +1,9 @@
+package ufsc.dso.trabalho1.Entidades;
+
+public class IntervaloInvalidoException extends Exception{
+
+    public IntervaloInvalidoException(String message) {
+        super(message);
+    }
+    
+}
