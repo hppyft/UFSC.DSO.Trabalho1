e41e10364eecf3e8f6d5b212851f58ec64cb7634
hppyft <leo@wiitec.com.br>
2016-06-08 03:56:29 -0300

    Update TelaControladorGeral.java

--- TelaControladorGeral.java
+++ TelaControladorGeral.java
@@ -1,53 +1,45 @@
 
 package ufsc.dso.trabalho1.Telas;
 import java.util.Scanner;
-import ufsc.dso.trabalho1.Entidades.DataInvalidaException;
+import ufsc.dso.trabalho1.Interfaces.Tela;
 
-public class TelaControladorGeral {
+public class TelaControladorGeral implements Tela{
     private Scanner scanner = new Scanner(System.in);
 
     public TelaControladorGeral() {
     }
-    
-    public String telaAtualizaHora () throws DataInvalidaException{
-        int hora = -1;
-        int minutos = -1;
-        System.out.println("Para iniciar o sistema, por favor digite o hora atual");
-        try{
-            hora = Integer.parseInt(scanner.nextLine());
-            while(hora < 0 || hora > 23){
-            
-                System.out.println("Voce digitou um valor incorreto. Para iniciar o sistema, por favor digite o hora atual");
-                hora = Integer.parseInt(scanner.nextLine());
-            }
-        }catch(NumberFormatException e){
-            throw new DataInvalidaException("Hora invalida.");
-        }
-        System.out.println("Para iniciar o sistema, por favor digite o minuto atual");
-        try{
-            minutos = Integer.parseInt(scanner.nextLine());
-            while(minutos < 0 || minutos > 59){
-                System.out.println("Voce digitou um valor incorreto. Para iniciar o sistema, por favor digite o minuto atual");
-                minutos = Integer.parseInt(scanner.nextLine());
-            }
-        }catch(NumberFormatException e){
-            throw new DataInvalidaException("Minuto invalido.");
-        }
-        String data = ""+hora+":"+minutos;
-        return data; 
-   }
-
-    public int mostraMenu() {
         
+    public int mostraMenu() {
+        System.out.println("");
+        System.out.println("");
         System.out.println("Bem Vindo ao sitema");
         System.out.println("");
+        System.out.println("");
         System.out.println("Selecione a opcao no menu a seguir:");
+        System.out.println("");
         System.out.println("-----------");
-        System.out.println("1 - Opcoes de acesso");
-        System.out.println("2 - Opcoes de cadastro");
-        System.out.println("3 - Atualizar hora");
-        System.out.println("4 - Fechar sistema");
+        System.out.println("");
+        System.out.println("1 - Opcoes de acesso e relatorios de negacao");
+        System.out.println("2 - Opcoes de funcionario (cadastro, atualizacoes, relatorios...)");
+        System.out.println("3 - Opcoes de cargos (cadastro, atualizacoes, relatorios...)");
+        System.out.println("4 - Atualizar hora do sistema");
+        System.out.println("5 - Atualizar data do sistema");
+        System.out.println("6 - Fechar sistema");
         
+        return scanner.nextInt();
+    }
+    
+    @Override
+    public void telaMensagem(String mensagem) {
+        System.out.println("");
+        System.out.println(mensagem);
+    }
+    
+    public int telaFecharSistema(){
+        System.out.println("");
+        System.out.println("tem certeza que deseja fechar o sistema");
+        System.out.println("1 - SIM");
+        System.out.println("2 - NAO");
         return scanner.nextInt();
     }
 }
