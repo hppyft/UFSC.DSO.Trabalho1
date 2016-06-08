5d148195790ff152dba90aa4889b00bc17ec75a1
hppyft <leo@wiitec.com.br>
2016-06-08 03:52:47 -0300

    Create CodigoInvalidoException.java

---
+++ CodigoInvalidoException.java
@@ -0,0 +1,10 @@
+
+package ufsc.dso.trabalho1.Entidades;
+
+public class CodigoInvalidoException extends Exception{
+
+    public CodigoInvalidoException(String message) {
+        super(message);
+    }
+    
+}
